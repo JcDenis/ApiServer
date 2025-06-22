7fce3391865a923a7b190d1718ec6c04f5435085
Jean-Christian Denis <contact@jcdenis.fr>
2025-06-22 11:14:19 +0200

    update translation

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -1,7 +1,7 @@
 <?php
 // Language: Français
-// Module: ApiServer - 0.3.2
-// Date: 2025-04-08 08:03:38
+// Module: ApiServer - 0.4
+// Date: 2025-06-22 09:13:12
 // Author: , contact@jcdenis.fr
 // Translated with Translater - 2025.03.03
 
@@ -21,10 +21,23 @@ L10n::$locales['No content'] = 'Pas de contenu';
 L10n::$locales['Not found'] = 'Non trouvé';
 L10n::$locales['Contribution is temporaly closed'] = 'Les contributions sont temporairement fermées';
 L10n::$locales['Submission failed'] = 'Echec de la soumission';
+L10n::$locales['Unauthorized'] = 'Non autorisé';
 L10n::$locales['API rate limit reach.'] = 'Limite de quota de l\'API atteinte.';
 L10n::$locales['API call limit per %d seconds:'] = 'Limite de nombre d\'appel à l\'API par %d secondes :';
 L10n::$locales['Enable public API for this blog'] = 'Activer l\'API public pour ce blog';
+L10n::$locales['Add user permission to use API on sign up'] = 'Ajouter les permissions utilisateur lors de l\'inscription';
 L10n::$locales['Reset anonymous API calls limit. (%d calls remaining for next %d seconds)'] = 'Réinitialiser les appels anonymes à l\'API. (%s appels restant pour les %d prochaines secondes)';
+L10n::$locales['One minute'] = 'Une minute';
+L10n::$locales['One hour'] = 'Une heure';
+L10n::$locales['One day'] = 'Un jour';
+L10n::$locales['One week'] = 'Une semaine';
+L10n::$locales['One month'] = 'Un mois';
+L10n::$locales['User token lifetime:'] = 'Durée de vie du jeton de l\'utilisateur :';
+L10n::$locales['Fifteen minutes'] = 'Quinze minutes';
+L10n::$locales['Half an hour'] = 'Une demi heure';
+L10n::$locales['Two hours'] = 'Deux heures';
+L10n::$locales['API calls rate limit period:'] = 'Période de remise à zéro de la limite d\'appels à l\'API :';
+L10n::$locales['API server cache lifetime:'] = 'Durée de vie du cache de l\'API :';
 L10n::$locales['Empty API cache directory'] = 'Vider le répertoire de cache de l\'API';
 L10n::$locales['API cache directory emptied.'] = 'Répertoire de cache de l\'API vidé';
 L10n::$locales['Failed to empty API cache directory.'] = 'Impossible de vider le répertoire de cache de l\'API';
